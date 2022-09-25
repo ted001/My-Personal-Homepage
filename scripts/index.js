cffbf6f08eb1138da8745a693ac5135e58d02be3
Zhiyi Jin <victoryjzy@gmail.com>
2022-09-24 21:52:39 -0700

    1.modify some css style 2.change the text color when the background color become dark

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,18 @@
 var btn = document.getElementById("theme-button");
+var intro = document.getElementById("intro-txt");
 var flag = 0;
 btn.onclick = function () {
     if (flag == 0) {
         document.body.style.backgroundColor = "#1D192E";
         btn.classList.remove("fa-moon");
         btn.classList.add("fa-sun");
+        intro.style.color = "#fff";
         flag = 1;
     } else {
         document.body.style.backgroundColor = "#fbfbfe";
         btn.classList.remove("fa-sun");
         btn.classList.add("fa-moon");
+        intro.style.color = "#40514e";
         flag = 0;
     }
 };
